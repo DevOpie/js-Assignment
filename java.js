1155a33f66e1284a47e2b82baef830ddfe57425f
Alawode Muneef Opeyemi <129981628+DevOpie@users.noreply.github.com>
2023-05-12 02:21:09 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+<script>
+      // Get the button and the text element
+      var button = document.querySelector('#update-me');
+      var text = document.querySelector('#text');
+      
+      // Add a click event listener to the button
+      button.addEventListener('click', function() {
+        // Change the text to "I'm Learning Javascript"
+        text.textContent = "I'm Learning Javascript"
+      });
+    </script>
